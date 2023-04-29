dcbc3a50dcff6599855cb30ab1f64befdc578bd0
Amy Sharin <amysharin1@gmail.com>
2023-04-29 15:27:09 -0400

    Some base code

---
+++ app.py
@@ -0,0 +1,32 @@
+import json
+
+from db import db
+from flask import Flask
+from flask import request
+import os
+
+db_filename = ""  # add
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+# generalized response formats
+def success_response(data, code=200):
+    """
+    Generalized success response function
+    """
+    return json.dumps(data), code
+
+
+def failure_response(message, code=404):
+    """
+    Generalized failure response function
+    """
+    return json.dumps({"error": message}), code
