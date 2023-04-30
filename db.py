0a12099d0d79efebc579ece32777711c5858ce32
Amy Sharin <amysharin1@gmail.com>
2023-04-30 16:38:14 -0400

    Added Vinyl Class

--- db.py
+++ db.py
@@ -33,7 +33,36 @@ class User(db.Model):
         }
 
 
-class Vinyl(db.Model):
+class Vinyl(db.Model):  # each vinyl associated with a user
     """
     Vinyl Model
     """
+    __tablename__ = "vinyl"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    artist = db.Column(db.String, nullable=False)
+    # optional to input songs
+    songs = db.relationship("Song", cascade="delete")
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        "user.id"), nullable=False)
+    # others...
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a Vinyl object
+        """
+        self.name = kwargs.get("name", "")
+        self.artist = kwargs.get("artist", "")
+        self.user_id = kwargs.get("user_id")
+
+    def serialize(self):
+        """
+        Serializes a Vinyl object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "artist": self.artist,
+            "songs": [s.seriealize() for s in self.songs],
+            "user_id": self.user_id
+        }
